43eac566f9e5a71e242cc7baa77bb034f769594d
andrewhopgood <andrewhopgood@rogers.com>
2020-10-08 15:53:05 -0400

    css fixes

--- src/components/LoseGame.js
+++ src/components/LoseGame.js
@@ -4,8 +4,8 @@ import "../styles/LoseGame.css";
 function LoseGame({ playAgain }) {
   return (
     <div id="win-game__container">
-      <div class="win__title">Sorry,</div>
-      <div class="win__title">Try again!</div>
+      <div className="win__title">Sorry,</div>
+      <div className="win__title">Try again!</div>
       <button id="btn__play-game" onClick={playAgain}>
         Play Again!
       </button>
